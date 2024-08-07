e608aca33f8c42d581714bc0e55d72f84cdc69f7
Cbstray <calebstraily01@gmail.com>
2024-08-07 15:18:59 -0500

    Code Efficiency Update
    Removed an older, more inefficient method of calculations for the program in favor of a quicker, neater method.
    
    Amounts for each coin are now tracked in a parallel array like the program's other counterparts.
    
    Removed the "coinCount" variable. The program now uses coinsAmount[i] in its stead. This will be reverted or revised if the program needs to keep a persistent track of the amounts for each coin type.

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 //Create a JavaScript program that calculates the total value of coins in US currency (pennies, nickels, dimes, and quarters) and sorts them into coin wrappers. 
 //The program will then output the number of coin wrappers needed and the total amount in dollars.
 
-let coinsAmount = [101, 34, 56, 64];
+//This program uses nested for loops to iterate and utilize a series of parallel arrays to calculate the total U.S. dollar amount of all the U.S. coins given in "coinsAmount".
+
+//Declare Variables
+let coinsAmount = [543, 45, 246, 1001];
 let coinType = ["Penny", "Nickel", "Dime", "Quarter"];
 let wrapperCapacity = [50, 40, 50, 40];
 let wrappersRequired = [0, 0, 0, 0];
 let dollarAmount = 0;
 
+//Runs for every value stored in the "coinsAmount" array
 for (let i = 0; i < coinsAmount.length; i++)
 {
-    let coinCount = 0;
-    let currentCoin = coinsAmount[i];
+    //Declare Variables
+    let currentCoinAmount = coinsAmount[i];
 
-    for (let j = 1; j <= currentCoin; j++)
+    //Runs for every coin in the current coin amount
+    for (let j = 1; j <= currentCoinAmount; j++)
     {
+        //Adds a coin's value based on the current iterated coin type and keeps track of the amount of c
         switch(true)
         {
             case (coinType[i] == "Penny"):
                 dollarAmount += 0.01;
-                coinCount++;
                 break;
             case (coinType[i] == "Nickel"):
                 dollarAmount += 0.05;
-                coinCount++;
                 break;
             case (coinType[i] == "Dime"):
                 dollarAmount += 0.10;
-                coinCount++;
                 break;
             case (coinType[i] == "Quarter"):
                 dollarAmount += 0.25;
-                coinCount++;
                 break;
             default:
                 console.log("Something went wrong.");
@@ -41,136 +43,26 @@ for (let i = 0; i < coinsAmount.length; i++)
     //Calculates the wrappers required for each coin type.
 
     //executes if the amount of coins exceeds the capacity of a single coin wrapper
-    if (coinCount > wrapperCapacity[i])
+    if (coinsAmount[i] > wrapperCapacity[i])
         {
             do
             {
                 wrappersRequired[i]++;
-                coinCount -= wrapperCapacity[i];
-            } while (coinCount > wrapperCapacity[i])
+                coinsAmount[i] -= wrapperCapacity[i];
+            } while (coinsAmount[i] > wrapperCapacity[i])
             
-            if (coinCount > 0)
+            if (coinsAmount[i] > 0)
             {
                 wrappersRequired[i]++;
             }
         }
         //executes if there are coins but only enough to contain in a single coin wrapper
-        else if (coinCount > 0 && coinCount <= wrapperCapacity[i])
+        else if (coinsAmount[i] > 0 && coinsAmount[i] <= wrapperCapacity[i])
         {
             wrappersRequired[i]++;
         }
 
         console.log(`${coinType[i]} wrappers required: ${wrappersRequired[i]}`);
-
-/*
-    switch(true)
-    {
-        //PENNIES CALCULATION
-        case (coinType[i] == "Penny"):
-            //executes if the amount of coins exceeds the capacity of a single coin wrapper
-            if (coinCount > wrapperCapacity[i])
-            {
-                do
-                {
-                    wrappersRequired[i]++;
-                    coinCount -= wrapperCapacity[i];
-                } while (coinCount > wrapperCapacity[i])
-                
-                if (coinCount > 0)
-                {
-                    wrappersRequired[i]++;
-                }
-            }
-            //executes if there are coins but only enough to contain in a single coin wrapper
-            else if (coinCount > 0 && coinCount <= wrapperCapacity[i])
-            {
-                wrappersRequired[i]++;
-            }
-
-            console.log(`${coinType[i]} wrappers required: ${wrappersRequired}`);
-            break;
-        
-        //NICKELS CALCULATION
-        case (coinType[i] == "Nickel"):
-            //executes if the amount of coins exceeds the capacity of a single coin wrapper
-            if (coinCount > nickelWrapperCapacity)
-            {
-                do
-                {
-                    nickelWrappersRequired++;
-                    coinCount -= nickelWrapperCapacity;
-                } while (coinCount > nickelWrapperCapacity)
-                
-                if (coinCount > 0)
-                {
-                    nickelWrappersRequired++;
-                }
-            }
-            //executes if there are coins but only enough to contain in a single coin wrapper
-            else if (coinCount > 0 && coinCount <= nickelWrapperCapacity)
-            {
-                nickelWrappersRequired++;
-            }
-
-            console.log("Nickel wrappers required: " + nickelWrappersRequired);
-            break;
-        
-        //DIMES CALCULATION
-        case (coinType[i] == "Dime"):
-            //executes if the amount of coins exceeds the capacity of a single coin wrapper
-            if (coinCount > dimesWrapperCapacity)
-            {
-                do
-                {
-                    dimesWrappersRequired++;
-                    coinCount -= dimesWrapperCapacity;
-                } while (coinCount > dimesWrapperCapacity)
-                
-                if (coinCount > 0)
-                {
-                    dimesWrappersRequired++;
-                }
-            }
-            //executes if there are coins but only enough to contain in a single coin wrapper
-            else if (coinCount > 0 && coinCount <= dimesWrapperCapacity)
-            {
-                dimesWrappersRequired++;
-            }
-
-            console.log("Dime wrappers required: " + dimesWrappersRequired);
-            break;
-        
-        //QUARTERS CALCULATION
-        case (coinType[i] == "Quarter"):
-            //executes if the amount of coins exceeds the capacity of a single coin wrapper
-            if (coinCount > quartersWrapperCapacity)
-            {
-                do
-                {
-                    quartersWrappersRequired++;
-                    coinCount -= quartersWrapperCapacity;
-                } while (coinCount > quartersWrapperCapacity)
-                
-                if (coinCount > 0)
-                {
-                    quartersWrappersRequired++;
-                }
-            }
-            //executes if there are coins but only enough to contain in a single coin wrapper
-            else if (coinCount > 0 && coinCount <= quartersWrapperCapacity)
-            {
-                quartersWrappersRequired++;
-            }
-
-            console.log("Quarter wrappers required: " + quartersWrappersRequired);
-            break;
-
-        //DEFAULT CASE
-        default:
-            console.log("Something went wrong.");
-            break;
-    }
-*/
 }
 
-console.log(`Dollar Amount: $${dollarAmount.toFixed(2)}`);
+console.log(`Total Dollar Amount: $${dollarAmount.toFixed(2)}`);
