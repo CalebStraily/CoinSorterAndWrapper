8cffa898cf8cc45c655606d60eae0a642c55b170
Cbstray <calebstraily01@gmail.com>
2024-08-09 10:48:25 -0500

    Coin Value Array & Lower Runtime
    Added the value of each coin to a parallel array called "coinValue". This allows the program to no longer need to run a for loop for each coin in coinsAmount[i], thus lowering runtime.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 //Declare Variables
 let coinsAmount = [543, 45, 246, 1001];
 let coinType = ["Penny", "Nickel", "Dime", "Quarter"];
+let coinValue = [0.01, 0.05, 0.10, 0.25];
 let wrapperCapacity = [50, 40, 50, 40];
 let wrappersRequired = [0, 0, 0, 0];
 let dollarAmount = 0;
@@ -16,53 +17,31 @@ for (let i = 0; i < coinsAmount.length; i++)
     //Declare Variables
     let currentCoinAmount = coinsAmount[i];
 
-    //Runs for every coin in the current coin amount
-    for (let j = 1; j <= currentCoinAmount; j++)
-    {
-        //Adds a coin's value based on the current iterated coin type and keeps track of the amount of c
-        switch(true)
-        {
-            case (coinType[i] == "Penny"):
-                dollarAmount += 0.01;
-                break;
-            case (coinType[i] == "Nickel"):
-                dollarAmount += 0.05;
-                break;
-            case (coinType[i] == "Dime"):
-                dollarAmount += 0.10;
-                break;
-            case (coinType[i] == "Quarter"):
-                dollarAmount += 0.25;
-                break;
-            default:
-                console.log("Something went wrong.");
-                break;
-        }
-    }
+    //multiplies the amount of coins at i by the coin's value at i
+    dollarAmount += (coinsAmount[i] * coinValue[i]);
 
     //Calculates the wrappers required for each coin type.
-
     //executes if the amount of coins exceeds the capacity of a single coin wrapper
     if (coinsAmount[i] > wrapperCapacity[i])
+    {
+        do
         {
-            do
-            {
-                wrappersRequired[i]++;
-                coinsAmount[i] -= wrapperCapacity[i];
-            } while (coinsAmount[i] > wrapperCapacity[i])
-            
-            if (coinsAmount[i] > 0)
-            {
-                wrappersRequired[i]++;
-            }
-        }
-        //executes if there are coins but only enough to contain in a single coin wrapper
-        else if (coinsAmount[i] > 0 && coinsAmount[i] <= wrapperCapacity[i])
+            wrappersRequired[i]++;
+            coinsAmount[i] -= wrapperCapacity[i];
+        } while (coinsAmount[i] > wrapperCapacity[i])
+        
+        if (coinsAmount[i] > 0)
         {
             wrappersRequired[i]++;
         }
+    }
+    //executes if there are coins but only enough to contain in a single coin wrapper
+    else if (coinsAmount[i] > 0 && coinsAmount[i] <= wrapperCapacity[i])
+    {
+        wrappersRequired[i]++;
+    }
 
-        console.log(`${coinType[i]} wrappers required: ${wrappersRequired[i]}`);
+    console.log(`${coinType[i]} wrappers required: ${wrappersRequired[i]}`);
 }
 
 console.log(`Total Dollar Amount: $${dollarAmount.toFixed(2)}`);
